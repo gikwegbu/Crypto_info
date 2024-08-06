c7363fac5f3f3622cdf42c7dc02af4d1283661d1
George  Ikwegbu Chinedu <g.ikwegbu@gmail.com>
2024-08-06 13:31:24 +0100

    Initial Commit

---
+++ Crypto_info/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto_info
+//
+//  Created by gikwegbu on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto_info/Crypto_infoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_infoApp.swift
+//  Crypto_info
+//
+//  Created by gikwegbu on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_infoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
